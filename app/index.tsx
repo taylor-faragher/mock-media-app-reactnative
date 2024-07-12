a2a39c9be3e40bf00d67203a6d38502a8888c92c
Taylor Faragher <faragher6@gmail.com>
2024-07-12 16:05:29 -0500

    Adds tailwind/nativewind css to project

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,13 @@
 import { Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import {Link} from 'expo-router';
 
-export default function Index() {
+export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen. Here is a change</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl">taylors test app</Text>
+      <StatusBar style='auto' />
+      <Link href='/profile' style={{ color: 'blue'}}> Go to Profile</Link>
     </View>
   );
 }
