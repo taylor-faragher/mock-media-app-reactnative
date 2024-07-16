05639dc8f1871f8b8ec1362f0e664f901cc9b8c1
Taylor Faragher <faragher6@gmail.com>
2024-07-15 22:32:16 -0500

    Minor code updates to trending component

--- components/Trending.tsx
+++ components/Trending.tsx
@@ -1,8 +1,15 @@
 import { useState } from "react";
-import { FlatList, ImageBackground, TouchableOpacity, Image } from "react-native";
-import * as Animatable from 'react-native-animatable';
 import { ResizeMode, Video } from "expo-av";
-import { icons } from "@/constants";
+import * as Animatable from "react-native-animatable";
+import {
+    FlatList,
+    Image,
+    ImageBackground,
+    TouchableOpacity,
+} from "react-native";
+
+import { icons } from "../constants";
+import { opacity } from "react-native-reanimated/lib/typescript/reanimated2/Colors";
 
 const zoomIn = {
     0: {
@@ -11,7 +18,7 @@ const zoomIn = {
     },
     1: {
         opacity: 1,
-        scale: 1.5,
+        scale: 1.1,
     },
 };
 
@@ -26,9 +33,9 @@ const zoomOut = {
     },
 };
 
-
-const TrendingItem = (activeItem, item) => {
+const TrendingItem = ({ activeItem, item }) => {
     const [play, setPlay] = useState(false);
+
     return (
         <Animatable.View
             className="mr-5"
@@ -36,43 +43,45 @@ const TrendingItem = (activeItem, item) => {
             duration={500}
         >
         {play ? (
-                <Video
-                    source={{ uri: item.video }}
-                    className="w-52 h-72 rounded-[33px] mt-3 bg-white/10"
-                    resizeMode={ResizeMode.CONTAIN}
-                    useNativeControls
-                    shouldPlay
-                    onPlaybackStatusUpdate={(status) => {
-                        if (status.didJustFinish) {
-                            setPlay(false);
-                        }
-                    }}
-                />
-            ) : (
-                <TouchableOpacity
-                    className="relative flex justify-center items-center"
-                    activeOpacity={0.7}
-                    onPress={() => setPlay(true)}
-                >
-                <ImageBackground
-                    source={{uri: activeItem.item.thumbnail}}
-                    className="w-52 h-72 rounded-[33px] my-5 overflow-hidden shadow-lg shadow-black/40"
-                    resizeMode="cover"
-                />
+            <Video
+                source={{ uri: item.video }}
+                className="w-52 h-72 rounded-[33px] mt-3 bg-white/10"
+                resizeMode={ResizeMode.CONTAIN}
+                useNativeControls
+                shouldPlay
+                onPlaybackStatusUpdate={(status) => {
+                    if (status.didJustFinish) {
+                        setPlay(false);
+                    }
+                }}
+            />
+        ) : (
+            <TouchableOpacity
+            className="relative flex justify-center items-center"
+            activeOpacity={0.7}
+            onPress={() => setPlay(true)}
+            >
+            <ImageBackground
+                source={{
+                uri: item.thumbnail,
+                }}
+                className="w-52 h-72 rounded-[33px] my-5 overflow-hidden shadow-lg shadow-black/40"
+                resizeMode="cover"
+            />
 
-                <Image
-                    source={icons.play}
-                    className="w-12 h-12 absolute"
-                    resizeMode="contain"
-                />
-                </TouchableOpacity>
-            )}
+            <Image
+                source={icons.play}
+                className="w-12 h-12 absolute"
+                resizeMode="contain"
+            />
+            </TouchableOpacity>
+        )}
         </Animatable.View>
-    )
-}
+    );
+};
 
-const Trending = ({posts}) => {
-    const [activeItem, setActiveItem] = useState(posts[1]);
+const Trending = ({ posts }) => {
+    const [activeItem, setActiveItem] = useState(posts[0]);
 
     const viewableItemsChanged = ({ viewableItems }) => {
         if (viewableItems.length > 0) {
@@ -84,17 +93,17 @@ const Trending = ({posts}) => {
         <FlatList
             data={posts}
             horizontal
-            renderItem={({item, index}) => (
-                <TrendingItem key={item.$id} activeItem={activeItem} item={item}/>
-            )}
             keyExtractor={(item) => item.$id}
+            renderItem={({item, index}) => (
+                <TrendingItem key={index} activeItem={activeItem} item={item} />
+            )}
             onViewableItemsChanged={viewableItemsChanged}
             viewabilityConfig={{
                 itemVisiblePercentThreshold: 70,
             }}
             contentOffset={{ x: 170, y: 0 }}
         />
-    )
-}
+    );
+};
 
 export default Trending;
