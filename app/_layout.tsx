69198a88666d6e9d7ac8dc069b7954fa11f147cb
Taylor Faragher <faragher6@gmail.com>
2024-07-15 22:24:50 -0500

    Adds auth, tabs, and search to router stack layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,7 @@
 import { Stack, SplashScreen } from "expo-router";
 import { useFonts } from "expo-font";
 import { useEffect } from "react";
+import GlobalProvider from "@/context/GlobalProvider";
 
 SplashScreen.preventAutoHideAsync();
 
@@ -26,9 +27,15 @@ export default function RootLayout() {
   }, [fontsLoaded, error])
 
   if(!fontsLoaded && !error) return null;
+  
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false}} />
-    </Stack>
+    <GlobalProvider>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false}} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false}} />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false}} />
+        <Stack.Screen name="search/[query]" options={{ headerShown: false}} />
+      </Stack>
+    </GlobalProvider>
   )
 }
